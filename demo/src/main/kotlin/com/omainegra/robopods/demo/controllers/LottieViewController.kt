2cfd02d79262ab7319329f8d6767fe5fbeb11d94
Omar Alejandro Mainegra Sarduy <omainegra@gmail.com>
2018-01-25 15:03:07 -0500

    Add lottie demo

--- demo/src/main/kotlin/com/omainegra/robopods/demo/controllers/LottieViewController.kt
+++ demo/src/main/kotlin/com/omainegra/robopods/demo/controllers/LottieViewController.kt
@@ -1,23 +1,33 @@
 package com.omainegra.robopods.demo.controllers
 
-import com.omainegra.robopods.sdwebimage.*
-import org.robovm.apple.foundation.NSObject
-import org.robovm.apple.foundation.NSOperationQueue
-import org.robovm.apple.foundation.NSURL
-import org.robovm.apple.uikit.*
+import com.omainegra.robopods.lottie.*
+import org.robovm.apple.uikit.UIImageView
+import org.robovm.apple.uikit.UIViewAutoresizing
+import org.robovm.apple.uikit.UIViewAutoresizing.FlexibleHeight
+import org.robovm.apple.uikit.UIViewAutoresizing.FlexibleWidth
+import org.robovm.apple.uikit.UIViewContentMode
+import org.robovm.apple.uikit.UIViewController
 import org.robovm.objc.annotation.CustomClass
-import org.robovm.objc.annotation.IBAction
 import org.robovm.objc.annotation.IBOutlet
 
 /**
  * Created by omainegra on 1/12/18.
  */
-@CustomClass("SDWebImageViewController")
+@CustomClass("LottieViewController")
 class LottieViewController : UIViewController() {
 
-    @IBOutlet lateinit var imageView: UIImageView
+    override fun viewDidLoad() {
+        super.viewDidLoad()
+        // Create Boat Animation
+        val boatAnimation = LOTAnimationView.animationNamed("Boat_Loader")
+        // Set view to full screen, aspectFill
+        boatAnimation.autoresizingMask = UIViewAutoresizing.with(FlexibleWidth, FlexibleHeight)
+        boatAnimation.contentMode = UIViewContentMode.ScaleAspectFill
+        boatAnimation.frame = view.bounds
+        boatAnimation.isLoopAnimation = true
+        boatAnimation.play()
 
-
-
-
+        // Add the Animation
+        view.addSubview(boatAnimation)
+    }
 }
